5ae403b798cd5e603e87901a26e5041439700ba6
padhikari5 <padhikari5@wisc.edu>
2026-03-08 21:11:03 -0500

    Fix checkstyle wildcard import errors

--- backend/src/main/java/com/project/auth/jwt/JwtService.java
+++ backend/src/main/java/com/project/auth/jwt/JwtService.java
@@ -1,6 +1,10 @@
 package com.project.auth.jwt;
 
-import io.jsonwebtoken.*;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
--- backend/src/main/java/com/project/controller/BoardController.java
+++ backend/src/main/java/com/project/controller/BoardController.java
@@ -1,7 +1,12 @@
 package com.project.controller;
+
 import com.project.model.dto.BoardResponse;
 import com.project.model.entity.Board;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 @RequestMapping("/api/board")
 @CrossOrigin(origins = "http://localhost:5173")
