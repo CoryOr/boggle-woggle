db2a6f4377542cf5920c6375254aa24452a0992f
rreichmann <rreichmann@wisc.edu>
2026-03-09 23:06:51 -0500

    finished styles

--- frontend/src/components/GameBoard/LetterTile.jsx
+++ frontend/src/components/GameBoard/LetterTile.jsx
@@ -1,3 +1,10 @@
+/**
+ * Tile component displaying a single letter on the game board
+ * 
+ * @param {string} letter - The letter to display on this tile
+ * @returns a styled tile with the given letter
+ */
+
 const LetterTile = ({letter}) => {
     return (
         <div className="letter-tile">
--- frontend/src/pages/GamePage.jsx
+++ frontend/src/pages/GamePage.jsx
@@ -1,32 +1,52 @@
 import "./Pages.css";
 import { useNavigate } from "react-router-dom";
 import { FaHourglass } from "react-icons/fa";
-import { Row, Col } from "react-bootstrap";
+import { useState, useEffect, useRef } from "react";
 import GameBoard from "../components/GameBoard/GameBoard";
-
+import "../components/GameBoard/GameBoard.css";
+/**
+ * The game page that user(s) will play on. It includes a couple of general labels for now. The main game logic is 
+ * currently handled in the GameBoard component.
+ * 
+ * @returns the page that a user will play a game on. 
+ * 
+ * TODOS: 
+ *   - Update timer to actually be functional
+ *   - Add word validation
+ */
 export default function GamePage() {
   const nav = useNavigate();
+  const [currentGuess, setCurrentGuess] = useState("");
+  const wordInputRef = useRef(null);
 
   const goHome = () => {
     nav("/");
   };
 
   return (
-    <>
-      <h1>DRAG OR TYPE LETTERS TO PLAY!</h1>
-      <Row className="align-items-center">
-        <Col xs="auto">
-          <FaHourglass />
-        </Col>
-        <Col xs="auto">
-          <p className="mb-0">2:52</p>
-        </Col>
-      </Row>
-      <p>WORD: ____________________</p>
+    <div className="game-page-container">
+      <p className="game-page-text title">DRAG OR TYPE LETTERS TO PLAY!</p>
+      <div className="timer-container">
+        <FaHourglass size="3rem" />
+        <p className="game-page-text">2:52</p>
+      </div>
+      <div className="score-container">
+        <p className="game-page-text">Score: 0</p>
+      </div>
+      <div className="word-input-container">
+        <label htmlFor="word-input" className="game-page-text">WORD:</label>
+        <input
+          ref={wordInputRef}
+          value={currentGuess}
+          onChange={(e) => setCurrentGuess(e.target.value)}
+          spellCheck={false}
+          className="word-input"
+        />
+      </div>
       <GameBoard />
       <button className="navButton" onClick={goHome}>
         Go back
       </button>
-    </>
+    </div>
   );
 }
