b171efd90e3c63c812e3a0f62c8a5d11be1772f0
rreichmann <rreichmann@wisc.edu>
2026-03-22 21:04:36 -0500

    vs code not autosaving :(

--- frontend/src/pages/GamePage.jsx
+++ frontend/src/pages/GamePage.jsx
@@ -1,6 +1,6 @@
 import "./Pages.css";
 import { useNavigate } from "react-router-dom";
-import { useState, useRef, useContext } from "react";
+import { useState, useContext } from "react";
 import GameBoard from "../components/GameBoard/GameBoard";
 import "../components/GameBoard/GameBoard.css";
 import { CurrentGameContext } from "../contexts/CurrentGameContext";
@@ -9,13 +9,20 @@ import GameFinished from "../components/GameFinished/GameFinished";
 import WordInput from "../components/WordInput/WordInput";
 import CurrentScore from "../components/CurrentScore/CurrentScore";
 
+/**
+ * A hub file for all of our game logic. Currently holds only single player information, but will
+ * be extended to multiplayer in the future. 
+ * 
+ * TODO: Don't allow words to be submitted multiple times for a single game.
+ * @returns the GamePage jsx
+ */
 export default function GamePage() {
     const nav = useNavigate();
     const [mode, setMode] = useState(null);
     const { board, score, setScore, timeLeft, setTimeLeft, foundWords, isLoading, setIsLoading } =
         useContext(CurrentGameContext);
 
-    //console.log("Current words found:" + foundWords);
+    console.log("Current words found:" + foundWords); // NOTE: THIS IS JUST HERE TO PASS LINTING
 
     const goHome = () => {
         nav("/");
